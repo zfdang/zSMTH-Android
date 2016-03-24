68b46cacab0ce7084879bad092f9f8c07a9b39c3
Zhengfa DANG <dantifer@gmail.com>
2016-03-24 11:50:15 +0800

    add comments, implement externalizable

--- app/src/main/java/com/zfdang/zsmth_android/models/Board.java
+++ app/src/main/java/com/zfdang/zsmth_android/models/Board.java
@@ -1,20 +1,28 @@
 package com.zfdang.zsmth_android.models;
 
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
 /**
  * Created by zfdang on 2016-3-14.
  */
-public class Board{
+public class Board implements Externalizable{
 
+    // 具体的版面
+    // http://www.newsmth.net/nForum/#!board/DrivingStudy
     private String boardID;
-    private String boardEngName;
-    private String boardChsName;
-    private String categoryName;
-    private String moderator;
+    private String boardEngName;  // Android
+    private String boardChsName;  // 安卓系统设备
+    private String categoryName;  // 电脑技术
+    private String moderator; // 版主
 
     // 可能是个版面的目录，而不是具体的版面
+    // http://www.newsmth.net/nForum/#!section/Automobile
     private boolean isFolder;
-    private String folderName;
-    private String folderID;
+    private String folderName;  // 汽车
+    private String folderID;    // Automobile
 
 
     public Board(String id, String chsName, String enName) {
@@ -86,4 +94,29 @@ public class Board{
                 '}';
     }
 
+    @Override
+    public void readExternal(ObjectInput input) throws IOException, ClassNotFoundException {
+        boardID = (String) input.readObject();
+        boardEngName = (String) input.readObject();
+        boardChsName = (String) input.readObject();
+        categoryName = (String) input.readObject();
+        moderator = (String) input.readObject();
+
+        isFolder = input.readBoolean();
+        folderID = (String) input.readObject();
+        folderName = (String) input.readObject();
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput output) throws IOException {
+        output.writeObject(boardID);
+        output.writeObject(boardEngName);
+        output.writeObject(boardChsName);
+        output.writeObject(categoryName);
+        output.writeObject(moderator);
+
+        output.writeBoolean(isFolder);
+        output.writeObject(folderID);
+        output.writeObject(folderName);
+    }
 }
