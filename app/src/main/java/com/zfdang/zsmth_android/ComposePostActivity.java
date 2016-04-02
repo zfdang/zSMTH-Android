5800c540ab0ad17bf58a37a18286c9f08c85247b
Zhengfa DANG <dantifer@gmail.com>
2016-04-02 15:52:12 +0800

    use MultiImageSelector to enable select image files

--- app/src/main/java/com/zfdang/zsmth_android/ComposePostActivity.java
+++ app/src/main/java/com/zfdang/zsmth_android/ComposePostActivity.java
@@ -1,10 +1,41 @@
 package com.zfdang.zsmth_android;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+
+import me.nereo.multi_image_selector.MultiImageSelectorActivity;
 
 public class ComposePostActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 653;
+    private static final String TAG = "ComposePostActivity";
+
+
+    private Button mButton;
+    private EditText mTitle;
+    private EditText mAttachments;
+    private EditText mContent;
+    private ArrayList<String> mPhotos;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_CODE){
+            if (resultCode == RESULT_OK && data != null) {
+                mPhotos = data.getStringArrayListExtra(MultiImageSelectorActivity.EXTRA_RESULT);
+                for (String filename: mPhotos) {
+                    Log.d(TAG, "onActivityResult: " + filename);
+                }
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +43,32 @@ public class ComposePostActivity extends AppCompatActivity {
         setContentView(R.layout.activity_compose_post);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mTitle = (EditText) findViewById(R.id.compose_post_title);
+        mAttachments = (EditText) findViewById(R.id.compose_post_attach);
+        mContent = (EditText) findViewById(R.id.compose_post_content);
+
+        mPhotos = new ArrayList<>();
+
+        mButton = (Button) findViewById(R.id.compose_post_attach_button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ComposePostActivity.this, MultiImageSelectorActivity.class);
+                // whether show camera
+                intent.putExtra(MultiImageSelectorActivity.EXTRA_SHOW_CAMERA, true);
+                // max select image amount
+                intent.putExtra(MultiImageSelectorActivity.EXTRA_SELECT_COUNT, 9);
+                // select mode (MultiImageSelectorActivity.MODE_SINGLE OR MultiImageSelectorActivity.MODE_MULTI)
+                intent.putExtra(MultiImageSelectorActivity.EXTRA_SELECT_MODE, MultiImageSelectorActivity.MODE_MULTI);
+                // default select images (support array list)
+                intent.putStringArrayListExtra(MultiImageSelectorActivity.EXTRA_DEFAULT_SELECTED_LIST, mPhotos);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+
+
+
 
     }
 
