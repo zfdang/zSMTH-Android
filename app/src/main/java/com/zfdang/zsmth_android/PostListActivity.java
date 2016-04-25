a863bb776f28a90d0427b015b6ead9e1fc727d53
Zhengfa DANG <dantifer@gmail.com>
2016-04-25 15:48:07 +0800

    add shareSDK library; init share function

--- app/src/main/java/com/zfdang/zsmth_android/PostListActivity.java
+++ app/src/main/java/com/zfdang/zsmth_android/PostListActivity.java
@@ -35,16 +35,18 @@ import com.jude.swipbackhelper.SwipeBackHelper;
 import com.thefinestartist.finestwebview.FinestWebView;
 import com.zfdang.SMTHApplication;
 import com.zfdang.zsmth_android.helpers.RecyclerViewUtil;
-import com.zfdang.zsmth_android.models.PostActionAlertDialogItem;
 import com.zfdang.zsmth_android.models.Board;
 import com.zfdang.zsmth_android.models.ComposePostContext;
 import com.zfdang.zsmth_android.models.Post;
+import com.zfdang.zsmth_android.models.PostActionAlertDialogItem;
 import com.zfdang.zsmth_android.models.PostListContent;
 import com.zfdang.zsmth_android.models.Topic;
 import com.zfdang.zsmth_android.newsmth.SMTHHelper;
 
 import java.util.List;
 
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 import okhttp3.ResponseBody;
 import rx.Observable;
 import rx.Subscriber;
@@ -485,13 +487,43 @@ public class PostListActivity extends AppCompatActivity
                     .show(url);
         } else if (which == 7) {
             // post_share
-            Toast.makeText(PostListActivity.this, "分享:TBD", Toast.LENGTH_SHORT).show();
+            // Toast.makeText(PostListActivity.this, "分享:TBD", Toast.LENGTH_SHORT).show();
+            sharePost(post);
 
         } else if (which == 8) {
             // post_delete_post
             Toast.makeText(PostListActivity.this, "删除:TBD", Toast.LENGTH_SHORT).show();
         }
 
+    }
+
+    public void sharePost(Post post) {
+        ShareSDK.initSDK(this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle(getString(R.string.share));
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+
+        // 启动分享GUI
+        oks.show(this);
     }
 
     @Override
