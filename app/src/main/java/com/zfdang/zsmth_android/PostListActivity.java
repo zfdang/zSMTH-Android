6689bf09733de820f99ebb20f6bee5fd9e9ae41c
Zhengfa Dang <dantifer@gmail.com>
2017-05-23 21:43:30 +0800

    upgrade library shareSDK

--- app/src/main/java/com/zfdang/zsmth_android/PostListActivity.java
+++ app/src/main/java/com/zfdang/zsmth_android/PostListActivity.java
@@ -655,20 +655,31 @@ public class PostListActivity extends SMTHBaseActivity
         // more information about OnekeyShare
         // http://wiki.mob.com/docs/sharesdk/android/cn/sharesdk/onekeyshare/OnekeyShare.html
 
-        // title标题，在印象笔记、邮箱、信息、微信（包括好友、朋友圈和收藏）、 易信（包括好友、朋友圈）、人人网和QQ空间使用
+        // title标题，印象笔记、邮箱、信息、微信、人人网、QQ和QQ空间使用
         oks.setTitle(title);
 
-        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        // titleUrl是标题的网络链接，仅在Linked-in,QQ和QQ空间使用
         // oks.setTitleUrl("http://sharesdk.cn");
 
         // text是分享文本，所有平台都需要这个字段
         oks.setText(content);
 
+        // 分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
         // imageUrl是图片的网络路径，新浪微博、人人网、QQ空间和Linked-In支持此字段
         oks.setImageUrl(imageURL);
 
-        // url在微信（包括好友、朋友圈收藏）和易信（包括好友和朋友圈）中使用
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+
+        // url仅在微信（包括好友和朋友圈）中使用
         oks.setUrl(postURL);
+
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+//        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+//        oks.setSite("ShareSDK");
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+//        oks.setSiteUrl("http://sharesdk.cn");
 
         // set callback functions
         oks.setCallback(new PlatformActionListener() {
---
+++ app/src/main/java/com/zfdang/zsmth_android/ddshare/DDShareActivity.java
@@ -0,0 +1,8 @@
+package com.zfdang.zsmth_android.ddshare;
+
+import cn.sharesdk.dingding.utils.DingdingHandlerActivity;
+
+
+public class DDShareActivity extends DingdingHandlerActivity {
+
+}
