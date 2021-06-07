d9bf4f3f6f70a0ca90a6c3ac3508d0daea52e958
zfdang <dantifer@gmail.com>
2021-06-07 14:03:52 +0800

    refine colors

--- app/src/main/java/com/zfdang/zsmth_android/models/Post.java
+++ app/src/main/java/com/zfdang/zsmth_android/models/Post.java
@@ -375,7 +375,7 @@ public class Post {
 
       // handle quoted content
       if (line.startsWith(":")) {
-        line = "<font color=#607D8B>" + line + "</font>";
+        line = "<font color=#299999>" + line + "</font>";
         sb.append(line).append("<br />");
         continue;
       }
