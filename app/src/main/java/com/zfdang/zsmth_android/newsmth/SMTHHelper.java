a1f9d848991fba47f6af1c71b8bf1cc649cac023
zfdang <dantifer@gmail.com>
2023-03-31 14:12:00 +0800

    update url

--- app/src/main/java/com/zfdang/zsmth_android/newsmth/SMTHHelper.java
+++ app/src/main/java/com/zfdang/zsmth_android/newsmth/SMTHHelper.java
@@ -73,11 +73,11 @@ public class SMTHHelper {
   public OkHttpClient mHttpClient;
 
   // WWW service of SMTH, but actually most of services are actually from nForum
-  static private final String SMTH_WWW_URL = "https://www.mysmth.net/";
-  static private final String SMTH_IMAGE_PREFIX_CDN = "https://static.mysmth.net";
-  static private final String SMTH_IMAGE_PREFIX_DIRECT = "https://www.mysmth.net";
+  static private final String SMTH_WWW_URL = "https://www.newsmth.net/";
+  static private final String SMTH_IMAGE_PREFIX_CDN = "https://static.newsmth.net";
+  static private final String SMTH_IMAGE_PREFIX_DIRECT = "https://www.newsmth.net";
   // Mobile service of SMTH; this is used for webchat sharing & open in browser
-  static public final String SMTH_MOBILE_URL = "https://m.mysmth.net";
+  static public final String SMTH_MOBILE_URL = "https://m.newsmth.net";
 
   public SMTHWWWService wService;
   static private final String SMTH_WWW_ENCODING = "GB2312";
