0a93daf252c62ef08f6d67d3580a0a6d2fcb6998
Zhengfa Dang <zfdang@users.noreply.github.com>
2023-04-18 23:05:16 +0800

    Update WebviewLoginActivity.java

--- app/src/main/java/com/zfdang/zsmth_android/WebviewLoginActivity.java
+++ app/src/main/java/com/zfdang/zsmth_android/WebviewLoginActivity.java
@@ -8,7 +8,7 @@ import android.webkit.WebView;
 public class WebviewLoginActivity extends SMTHBaseActivity {
 
     private WebView mWebView;
-    private String url = "https://m.mysmth.net/index";
+    private String url = "https://m.newsmth.net/index";
 
     private String username;
     private String password;
