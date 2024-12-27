d337da1449ea9351ebe50b2993418223c2c3286d
dantifer <dantifer@gmail.com>
2024-12-27 21:32:15 +0800

    Create a trust manager that does not validate certificate chains

--- app/src/main/java/com/zfdang/zsmth_android/newsmth/SMTHHelper.java
+++ app/src/main/java/com/zfdang/zsmth_android/newsmth/SMTHHelper.java
@@ -37,12 +37,20 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.security.cert.CertificateException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import io.reactivex.Observable;
 import io.reactivex.annotations.NonNull;
@@ -110,6 +118,50 @@ public class SMTHHelper {
     return result;
   }
 
+  // https://stackoverflow.com/questions/25509296/trusting-all-certificates-with-okhttp
+  private static OkHttpClient getUnsafeOkHttpClient() {
+    try {
+      // Create a trust manager that does not validate certificate chains
+      final TrustManager[] trustAllCerts = new TrustManager[] {
+              new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                }
+
+                @Override
+                public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                }
+
+                @Override
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                  return new java.security.cert.X509Certificate[]{};
+                }
+              }
+      };
+
+      // Install the all-trusting trust manager
+      final SSLContext sslContext = SSLContext.getInstance("SSL");
+      sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+      // Create an ssl socket factory with our all-trusting manager
+      final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+      OkHttpClient.Builder builder = new OkHttpClient.Builder();
+      builder.sslSocketFactory(sslSocketFactory, (X509TrustManager)trustAllCerts[0]);
+      builder.hostnameVerifier(new HostnameVerifier() {
+        @Override
+        public boolean verify(String hostname, SSLSession session) {
+          return true;
+        }
+      });
+
+      OkHttpClient okHttpClient = builder.build();
+      return okHttpClient;
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+
   // protected constructor, can only be called by getInstance
   protected SMTHHelper() {
 
@@ -122,7 +174,7 @@ public class SMTHHelper {
     int cacheSize = 250 * 1024 * 1024; // 250 MiB
     Cache cache = new Cache(httpCacheDirectory, cacheSize);
 
-    mHttpClient = new OkHttpClient().newBuilder().addInterceptor(logging).addInterceptor(new Interceptor() {
+    mHttpClient = getUnsafeOkHttpClient().newBuilder().addInterceptor(logging).addInterceptor(new Interceptor() {
       @Override public Response intercept(Chain chain) throws IOException {
         Request request = chain.request().newBuilder().header("User-Agent", USER_AGENT).build();
         return chain.proceed(request);
